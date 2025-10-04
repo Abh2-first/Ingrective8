a7b03017c916db9919bf12c76a5dcbd3be5e0797
Abh2-first <abhaykaliyar20425@gmail.com>
2025-10-04 14:34:38 +0530

    Create contact.js

---
+++ styles/components/pages/pages/pages/pages/contact.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+export default function Contact() {
+  return (
+    <>
+      <Navbar />
+      <div style={{padding:"30px"}}>
+        <h1>Contact Us</h1>
+        <p>Email: support@ingrective.ai</p>
+      </div>
+    </>
+  );
+}
